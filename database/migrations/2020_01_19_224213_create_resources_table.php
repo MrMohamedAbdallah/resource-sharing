551db620b355db5f0813d0fc86ff5045d3689136
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-01-20 00:46:06 +0200

    Setup database connection & migrations

---
+++ database/migrations/2020_01_19_224213_create_resources_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("url");
+            $table->string("name");
+            $table->unsignedBigInteger("stack_id");
+            $table->unsignedBigInteger("category_id");
+            $table->foreign("stack_id")->references("id")->on("stacks");
+            $table->foreign("category_id")->references("id")->on("categories");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resources');
+    }
+}
