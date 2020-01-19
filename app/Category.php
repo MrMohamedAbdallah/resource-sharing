0feb88d28d49833beb94b2742fb42de38b6bc70f
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-01-20 01:21:23 +0200

    Solve table name in Category controller
    Table name in Category controller was spelled wrong which causes error
    with connection to the DB.

--- app/Category.php
+++ app/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected   $table = "cateogries",
+    protected   $table = "categories",
                 $primaryKey = "id",
                 $fillable   = [
                     "name"
